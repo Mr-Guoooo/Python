eb94eff1ab907bdaf56ce2971906e5a83d87520e
Freedom <Mr_Guoooo@aliyun.com>
2019-10-09 10:13:33 +0800

    添加数字判断

--- check.py
+++ check.py
@@ -27,4 +27,20 @@ def is_IP(ipAddr):
     else:  
         return False
 
+def is_number(s):
+    "检查所给字符串是否为数字"
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError,ValueError):
+        pass
+    return False
+
 
